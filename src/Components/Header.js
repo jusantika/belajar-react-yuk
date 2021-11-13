f644619bdca28811fa6233053737b5cb2ffe0981
jusantika <juanjusan4@gmail.com>
2021-11-13 18:58:19 +0800

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import bg from 'https://raw.githubusercontent.com/Maclinz/yt_animated-landing-page/b9dd0913bf1b3f8b4226d7eead2dddf68dfcfa94/src/img/bg.svg';
 
 function Header() {
   return (
-    <div>
+    <HeaderStyled>
       <h1>Gimana Gengs?</h1>
-    </div>
+    </HeaderStyled>
   );
 }
 
 const HeaderStyled = styled.header`
   height: 100vh;
   width: 100%;
+  background-image: url('https://raw.githubusercontent.com/Maclinz/yt_animated-landing-page/b9dd0913bf1b3f8b4226d7eead2dddf68dfcfa94/src/img/bg.svg');
 `;
 
 export default Header;
