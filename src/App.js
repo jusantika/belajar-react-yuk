3c7b867c42ad2eb893ae7eaab1314e7dde903cd8
jusantika <juanjusan4@gmail.com>
2021-11-13 19:19:03 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
-import { OuterLayout } from '/styles/Layouts';
+import { OuterLayout } from './styles/Layouts';
 
 export default function App() {
   return (
