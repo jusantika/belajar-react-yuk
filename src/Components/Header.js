ea02c5dc38c4b9170a87f2675fcdecd8a39ce6f6
jusantika <juanjusan4@gmail.com>
2021-11-13 19:04:00 +0800

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import bg from 'https://raw.githubusercontent.com/Maclinz/yt_animated-landing-page/b9dd0913bf1b3f8b4226d7eead2dddf68dfcfa94/src/img/bg.svg';
+import bg from 'https://github.com/Maclinz/yt_animated-landing-page/blob/master/src/img/bg.svg';
 
 function Header() {
-  return <HeaderStyled></HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <div>
+        <h6>Main Lost Saga</h6>
+      </div>
+    </HeaderStyled>
+  );
 }
 
 const HeaderStyled = styled.header`
