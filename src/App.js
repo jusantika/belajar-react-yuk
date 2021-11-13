a7aef03171a7f7943904b4f4f6186e9acf618f6b
jusantika <juanjusan4@gmail.com>
2021-11-13 18:41:16 +0800

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Header from './Components/Header';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function header() {
+  return (
+    <div>
+      <h1>Gimana Gengs?</h1>
+    </div>
+  );
+}
+
+const HeaderStyled = styled.header`
+  height: 100vh;
+  width: 100%;
+`;
+
+export default Header;
