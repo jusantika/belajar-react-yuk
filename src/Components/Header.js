537d6359f61251e37e7e109650e9abb8854e7eed
jusantika <juanjusan4@gmail.com>
2021-11-13 19:09:02 +0800

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import bg from 'https://raw.githubusercontent.com/Maclinz/yt_animated-landing-page/b9dd0913bf1b3f8b4226d7eead2dddf68dfcfa94/src/img/bg.svg';
 
 function Header() {
   return (
@@ -15,7 +14,7 @@ function Header() {
 const HeaderStyled = styled.header`
   height: 100vh;
   width: 100%;
-  background-image: url(${bg});
+  background-image: url('https://raw.githubusercontent.com/Maclinz/yt_animated-landing-page/b9dd0913bf1b3f8b4226d7eead2dddf68dfcfa94/src/img/bg.svg');
 `;
 
 export default Header;
